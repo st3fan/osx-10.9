627cae43d021653a87256f64e71038b85cf7d4cb
Stefan Arentz <stefan@arentz.ca>
2013-11-08 16:53:05 -0500

    Added emacs-92

---
+++ emacs-92/emacs/src/s/windows95.h
@@ -0,0 +1,8 @@
+/* System description file for Windows 95.  */
+
+#include "windowsnt.h"
+
+#define WINDOWS95
+
+/* arch-tag: 8a37be6f-312c-4b2a-919e-58a71a0fb4b3
+   (do not change this comment) */
