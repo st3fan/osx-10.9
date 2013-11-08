9987115380160a3524f24508447b22536c05086b
Stefan Arentz <stefan@arentz.ca>
2013-11-08 16:35:58 -0500

    Added ICU-511.25

---
+++ ICU-511.25/icuSources/data/coll/collocal.mk
@@ -0,0 +1,9 @@
+#
+# Local Apple addition for collation resources
+# Copyright (c) 2004-2005, 2007, 2012-2013 Apple Inc. All rights reserved.
+#
+
+COLLATION_SOURCE_LOCAL = ckb.txt iu.txt iu_Cans.txt iu_Cans_CA.txt\
+	ky.txt ky_KG.txt mn.txt mn_Cyrl.txt mn_Cyrl_MN.txt\
+	ms_Arab.txt ms_Arab_BN.txt ms_Arab_MY.txt tg.txt tg_Cyrl.txt\
+	tg_Cyrl_TJ.txt tk.txt tk_Latn.txt tk_Latn_TM.txt
---
+++ ICU-511.25/icuSources/data/rbnf/rbnflocal.mk
@@ -0,0 +1,7 @@
+#
+# Local Apple addition for rbnf resources
+# Copyright (c) 2005, 2007, 2012-2013 Apple Inc. All rights reserved.
+#
+
+#RBNF_SOURCE_LOCAL = ky.txt
+RBNF_SOURCE_LOCAL =
