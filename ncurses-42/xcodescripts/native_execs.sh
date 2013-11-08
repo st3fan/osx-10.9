816945d00f46284e99f351dd1c61385764b253e3
Stefan Arentz <stefan@arentz.ca>
2013-11-08 17:05:23 -0500

    Added ncurses-42

---
+++ ncurses-42/xcodescripts/native_execs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e -x
+
+SDK="-sdk macosx"
+
+export -n SDKROOT
+export -n ARCHS CURRENT_ARCH RC_ARCHS RC_CFLAGS
+export -n RC_TARGET_CONFIG RC_TARGET_UPDATE EMBEDDED_PROFILE_NAME
+# Remove all platform variables from the environment
+export -n `env | grep -E "^PLATFORM_" | cut -d= -f1` \
+	
+# Build the native_tic target using the current SDK for the build host
+xcodebuild \
+	"$@" \
+	$SDK \
+	SYMROOT="$SYMROOT" \
+	DSTROOT="$BUILT_PRODUCTS_DIR" \
+	install
