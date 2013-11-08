997e254242077716847292db3414f5b721b1ce19
Stefan Arentz <stefan@arentz.ca>
2013-11-08 17:30:50 -0500

    Added vim-53

---
+++ vim-53/local/vimrc
@@ -0,0 +1,12 @@
+" Configuration file for vim
+set modelines=0		" CVE-2007-2438
+
+" Normally we use vim-extensions. If you want true vi-compatibility
+" remove change the following statements
+set nocompatible	" Use Vim defaults instead of 100% vi compatibility
+set backspace=2		" more powerful backspacing
+
+" Don't write backup file if vim is being called by "crontab -e"
+au BufWrite /private/tmp/crontab.* set nowritebackup
+" Don't write backup file if vim is being called by "chpass"
+au BufWrite /private/etc/pw.* set nowritebackup
---
+++ vim-53/src/auto/pathdef.c
@@ -0,0 +1,10 @@
+/* pathdef.c */
+/* This file is automatically created by Makefile
+ * DO NOT EDIT!  Change Makefile only. */
+#include "vim.h"
+char_u *default_vim_dir = (char_u *)"/usr/share/vim";
+char_u *default_vimruntime_dir = (char_u *)"";
+char_u *all_cflags = (char_u *)"gcc -c -I. -D_FORTIFY_SOURCE=0 -Iproto -DHAVE_CONFIG_H -arch i386 -arch x86_64 -g -Os -pipe";
+char_u *all_lflags = (char_u *)"gcc -arch i386 -arch x86_64 -o vim -lncurses";
+char_u *compiled_user = (char_u *)"root";
+char_u *compiled_sys = (char_u *)"apple.com";
